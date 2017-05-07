9f6219d4f44325479ee0c78c60301e6c27105049
LarisMourullo <larismourullo@live.com>
2017-05-06 22:45:41 -0300

    Alteração Solução Comparação Profunda

--- JavaScript/Comparação Profunda/Solução.js
+++ JavaScript/Comparação Profunda/Solução.js
@@ -25,8 +25,5 @@ function deepEqual(valueOne, valueTwo) {
 
 var obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
-// → true
 console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
